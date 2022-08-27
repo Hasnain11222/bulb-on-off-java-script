99f867ae20ee7914fc160100e0ca264f143a1786
Hasnain11222 <109806090+Hasnain11222@users.noreply.github.com>
2022-08-28 01:05:43 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// on function
+
+function TTP(){
+    let bulbobj = document.querySelector(".bulboff");
+   bulbobj.setAttribute("src", "image/bulb-on.png");
+}
+
+
+function bulboff(){
+    let bulbobj = document.querySelector(".bulboff");
+   bulbobj.setAttribute("src", "image/bulb-off.png");
+}
